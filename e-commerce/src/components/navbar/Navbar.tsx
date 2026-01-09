ffdfa63f3c963d41a27d9a86c0d301eedb41df49
Marcos Gallardi <marcos.gallardi@grupo-sanmartin.com>
2026-01-09 14:08:31 -0300

    avanzando

--- e-commerce/src/components/navbar/Navbar.tsx
+++ e-commerce/src/components/navbar/Navbar.tsx
@@ -2,12 +2,21 @@ import { ItemNavbar } from './ItemNavbar'
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-      <ItemNavbar name="Inicio" link="/" />
-      <ItemNavbar name="Cursos" link="/cursos" />
-      <ItemNavbar name="Nosotros" link="/nosotros" />
-      <ItemNavbar name="Ustedes" link="/ustedes" />
-      <ItemNavbar name="Contacto" link="/contacto" />
-    </div>
+    <>
+      <div className="d-none d-lg-block">
+        <div className='navbar'>
+          <ItemNavbar name="Inicio" link="/" />
+          <ItemNavbar name="Cursos" link="/cursos" />
+          <ItemNavbar name="Nosotros" link="/nosotros" />
+          <ItemNavbar name="Ustedes" link="/ustedes" />
+          <ItemNavbar name="Contacto" link="/contacto" />
+        </div>
+      </div>
+
+      <div className="d-lg-none navbar">
+        <i className="bi bi-filter-left icon-navbar"></i>
+      </div>
+
+    </>
   )
 }
