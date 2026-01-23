df2729ea272b2d41a31d54b3510f20b34510ce83
Marcos Gallardi <marcos.gallardi@grupo-sanmartin.com>
2026-01-23 13:15:17 -0300

    avanzando

--- e-commerce/src/components/navbar/Navbar.tsx
+++ e-commerce/src/components/navbar/Navbar.tsx
@@ -20,25 +20,23 @@ export const Navbar = () => {
       <div className="d-lg-none navbar-1">
         <i className="bi bi-filter-left icon-navbar" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"></i>
       </div>
-      <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div className="offcanvas offcanvas-start w-75 navbar-1-style" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div className="offcanvas-header">
           <img src={logoImg1} alt="" className='logoImg' />
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
-          <div>
-            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-          </div>
-          <div className="dropdown mt-3">
-            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-              Dropdown button
-            </button>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </div>
+          <hr />
+          <ItemNavbar name="Inicio" link="/" />
+          <hr />
+          <ItemNavbar name="Cursos" link="/cursos" />
+          <hr />
+          <ItemNavbar name="Nosotros" link="/nosotros" />
+          <hr />
+          <ItemNavbar name="Ustedes" link="/ustedes" />
+          <hr />
+          <ItemNavbar name="Contacto" link="/contacto" />
+          <hr />
         </div>
       </div>
     </>
