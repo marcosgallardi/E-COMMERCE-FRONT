6d0c4950064161b60e1e5804f8fa889dcb9967ca
Marcos Gallardi <marcos.gallardi@grupo-sanmartin.com>
2026-01-30 17:02:49 -0300

    avanzando

--- e-commerce/src/components/nosotros/Nosotros.tsx
+++ e-commerce/src/components/nosotros/Nosotros.tsx
@@ -1,4 +1,5 @@
 import "../nosotros/Nosotros.css"
+import imgNosotros from "../../assets/images/Home1.svg"
 
 export const Nosotros = () => {
     return (
@@ -6,6 +7,15 @@ export const Nosotros = () => {
             <div className="nosotros">
 
                 <h2>Nosotros</h2>
+                <div className="container containerNosotros">
+                    <img src={imgNosotros} alt="" className="imgNosotros" />
+                    <div>
+                        <h2>holanda</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet impedit, vitae voluptatum alias, veniam enim sapiente optio aliquam dolorum fugiat culpa consequuntur nulla blanditiis cum! Excepturi maiores optio hic at?</p>
+                    </div>
+
+                </div>
+
             </div>
         </>
     )
